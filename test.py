355a60f28620298cc24813948054474c652193cf
Jakub Hanslik <jakub@hanslikovi.com>
2024-11-21 11:19:37 +0100

    :)

--- test.py
+++ test.py
@@ -8,3 +8,11 @@ def fibonacci(n):
     for _ in range(2, n + 1):
         a, b = b, a + b
     return b
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
