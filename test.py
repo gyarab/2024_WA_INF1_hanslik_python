3c582c521d857f01fe7b56005aa858f10e0d3fb1
Jakub Hanslik <jakub@hanslikovi.com>
2024-11-21 11:11:40 +0100

    Tkinter removed

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
 def fibonacci(n):
-    import tkinter as tk
-    import time
-
-    # Create the main application window
-    root = tk.Tk()
-    root.title("Tkinter")
-    root.geometry("300x200")  # Set the size of the window (width x height)
-
-    # Add a label to the window
-    label = tk.Label(root, text=":)", font=("Arial", 14))
-    label.pack(pady=20)  # Add padding around the label
-
-    # Run the application
-    root.mainloop()
-
-    time.sleep(4)
-    exit()
-
     if n <= 0:
         return 0
     elif n == 1:
